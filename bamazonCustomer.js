c62c3f4691bdaa302b26013a818e1a9173f8d342
cecileanne <cecileannesison@gmail.com>
2019-10-19 15:33:43 -0500

    added shopping cart function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { table } = require("table");
+const chalk = require("chalk");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -22,15 +24,30 @@ connection.connect(function(err) {
 });
 
 function afterConnection() {
-  const optionArray = [];
+  const optionArray = [["ID#", "Product", "Cost/Unit", "Amount in Stock"]];
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     res.forEach(product =>
-      optionArray.push(
-        `ID#: ${product.id}, ${product.product_name}, Price: $${product.price}/unit`
+      optionArray.push([
+        product.id,
+        product.product_name,
+        `$${product.price}`,
+        `${product.stock_quantity} available!`
+      ])
+    );
+    let options = {
+      drawHorizontalLine: (index, size) => {
+        return index === 0 || index === 1 || index === size;
+      }
+    };
+
+    output = table(optionArray, options);
+    console.log(
+      chalk.black.bgCyan(
+        `\n\nWelcome to Bamazon - A store for all of your shopping needs`
       )
     );
-    console.log(optionArray);
+    console.log(output);
 
     function userShop() {
       inquirer
@@ -42,28 +59,55 @@ function afterConnection() {
           }
         ])
         .then(answers => {
-          inquirer.prompt([
-            {
-              type: "input",
-              message: `How many of ID# ${answers.productID} would you like to buy?`,
-              name: "userQuantity"
-            }
-          ]); // closes second prompt
-        }) // closes then
-        .then(response => {
-          purchaseItem(answers.productID, response.userQuantity);
-        })
-        .finally(response => {
-          connection.query(
-            `SELECT ${response.productID} FROM products;`,
-            function(err, data) {
-              if (err) throw err;
-            }
-          );
-          connection.end();
-        })
-        .catch(err => console.log(err)); // closes then
-    } // closes userShop
+          const productID = answers.productID;
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: `How many of ID#${answers.productID} would you like to buy?`,
+                name: "userQuantity"
+              }
+            ]) // closes second prompt
+            .then(response => {
+              // TO DO check against amount to see if we have enough
+              // if no then console.log(`Sorry, we don't have enough in stock - please try again with a smaller amount or come back again`)
+              // update shopping cart
+              function addToCart() {
+                // set variables
+                const userQuantity = response.userQuantity;
+                const productPrice = optionArray[answers.productID - 1].price;
+                // TO DO fix this balance
+                const balance = Number(response.userQuantity * productPrice);
+                connection.query(
+                  "INSERT INTO shopping_cart SET ?",
+                  {
+                    id: productID,
+                    quantity_wanted: userQuantity
+                  },
+                  function(err, response) {
+                    if (err) throw err;
+                    console.log(
+                      `\n${userQuantity} of ID#${productID} added to your cart!\nYour current balance for this purchase is $${balance}\n`
+                    );
+                  }
+                );
+              } // closes connection query
+              addToCart(answers.productID, response.userQuantity);
+            }) // closes then
+            .finally(response => {
+              // // CHECK OUT is a joining of the tables
+              // connection.query(
+              //   `SELECT ${response.productID} FROM products;`,
+              //   function(err, data) {
+              //     if (err) throw err;
+              //   }
+              // );
+              // // TO DO give option of adding more to the cart
+              connection.end();
+            })
+            .catch(err => console.log(err)); // closes then
+        });
+    }
     userShop();
   });
 }
